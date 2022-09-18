851840669ad15b855548c7cdc04221a622653bff
SirawichDev <sirawit0676@gmail.com>
2022-09-18 19:30:04 +0700

    feat(dime): :sparkles: download store component

---
+++ components/DownloadStore.vue
@@ -0,0 +1,11 @@
+<template>
+ <div class="flex w-full justify-center gap-10">
+    <AppStore classes="h-auto w-[216px] cursor-pointer"/>
+    <PlayStore classes="h-auto w-[216px] cursor-pointer"/>
+</div>
+</template>
+<script setup lang="ts">
+import AppStore from '@/components/icons/AppStore.vue';
+import PlayStore from '@/components/icons/PlayStore.vue';
+
+</script>
