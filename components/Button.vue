d83598292b963096f1d6c5c6ec09a91c250a70bf
SirawichDev <sirawit0676@gmail.com>
2022-09-21 20:24:30 +0700

    perf: make it better

--- components/Button.vue
+++ components/Button.vue
@@ -1,13 +1,14 @@
 <template>
-    <button :class="`${themeProps[props.theme]} ${classes ?? ''}`">
-        <slot></slot>
-    </button>
+  <button :class="`${themeProps[props.theme]} ${classes ?? ''}`">
+    <slot></slot>
+  </button>
 </template>
 <script lang="ts" setup>
 import { defineProps } from 'vue'
+
 const themeProps = {
-    rectangle: 'py-2 bg-black text-white rounded-xl cursor-pointer',
-    rounded: 'py-2 bg-black text-white rounded-full cursor-pointer'
+  rectangle: 'py-2 bg-black text-white rounded-xl cursor-pointer',
+  rounded: 'py-2 bg-black text-white rounded-full cursor-pointer',
 }
-const props = defineProps<{ classes?: string, theme: string}>()
+const props = defineProps<{ classes?: string; theme: string }>()
 </script>
