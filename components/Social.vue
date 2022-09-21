68b94b6646c56ac5cf1dc59418084c2ef35abcd2
SirawichDev <sirawit0676@gmail.com>
2022-09-21 23:42:30 +0700

    DIME: social component added

---
+++ components/Social.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="flex gap-5">
+    <Facebook />
+    <Tiktok />
+    <Instagram />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import Facebook from '@/components/icons/Facebook.vue'
+import Tiktok from '@/components/icons/Tiktok.vue'
+import Instagram from '@/components/icons/Instagram.vue'
+</script>
+
+<style scoped></style>
