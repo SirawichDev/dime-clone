3968624c80422d4e180fb71fa7ffb4827cf1f1a5
SirawichDev <sirawit0676@gmail.com>
2022-09-21 23:44:43 +0700

    DIME: mobile responsive

--- components/Text.vue
+++ components/Text.vue
@@ -8,12 +8,14 @@
 import { defineProps } from 'vue'
 
 const textProps = {
-  heading: 'text-6xl font-bold',
-  subHeading: 'text-lg font-normal',
-  subHeading2: 'text-l font-normal',
+  heading: 'text-5xl lg:text-6xl font-bold',
+  subHeading: 'text-l lg:text-lg font-normal',
+  subHeading2: 'text-l',
   small: 'text-sm',
   topic: 'text-5xl font-bold',
   subTopic: 'text-3xl font-bold',
+  subTopic2: 'text-2xl',
+  subTopic3: 'text-xl',
 }
 const props = defineProps<{ theme: string; classes?: string }>()
 </script>
