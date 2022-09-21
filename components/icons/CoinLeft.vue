dbb252ccb248f86930944440ba277f7c791589b9
SirawichDev <sirawit0676@gmail.com>
2022-09-21 20:35:10 +0700

    DIME: reformat

--- components/icons/CoinLeft.vue
+++ components/icons/CoinLeft.vue
@@ -1,7 +1,11 @@
 <template>
-    <img :class="props.classes ?? ''" src="https://dime.co.th/left-coin-lg.webp" alt="coin-left"/>
-    </template>
-    <script setup lang="ts">
-    import { defineProps } from 'vue'
-    const props = defineProps<{ classes: string}>()
-    </script>
+  <img
+    :class="props.classes ?? ''"
+    src="https://dime.co.th/left-coin-lg.webp"
+    alt="coin-left"
+  />
+</template>
+<script setup lang="ts">
+import { defineProps } from 'vue'
+const props = defineProps<{ classes: string }>()
+</script>
