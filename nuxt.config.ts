aba3fa76340a7d2caa6147fa01edebfca3b970f2
SirawichDev <sirawit0676@gmail.com>
2022-09-21 20:38:55 +0700

    DIME: reformat

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,11 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    windicss: {
-        analyze: true,
-        config: {
-            attributify: true,
-        },
+  windicss: {
+    analyze: true,
+    config: {
+      attributify: true,
     },
-    css: ['@/assets/font.css'],
-    modules: ['nuxt-windicss'],
+  },
+  css: ['@/assets/font.css'],
+  modules: ['nuxt-windicss'],
 })
