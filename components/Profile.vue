af5ba73685065e6265f0bdb5bde38c53007cade5
SirawichDev <sirawit0676@gmail.com>
2022-09-21 19:24:54 +0700

    re-format

--- components/Profile.vue
+++ components/Profile.vue
@@ -1,16 +1,18 @@
 <template>
-<div class="flex gap-5">
+  <div class="flex gap-5">
     <div class="relative">
-    <img class="w-25 rounded-full h-25" src="https://png.pngitem.com/pimgs/s/506-5067022_sweet-shap-profile-placeholder-hd-png-download.png"/>
-</div>
-<div class="flex flex-col">
-    <Text theme="subHeading" classes="font-semibold">{{ writer }}</Text>
-    <Text theme="subHeading2" classes="font-normal text-gray-400"> {{ position }}</Text>
-</div>
-</div>
+      <img class="w-25 rounded-full h-25"
+           src="https://png.pngitem.com/pimgs/s/506-5067022_sweet-shap-profile-placeholder-hd-png-download.png"/>
+    </div>
+    <div class="flex flex-col">
+      <Text theme="subHeading" classes="font-semibold">{{ writer }}</Text>
+      <Text theme="subHeading2" classes="font-normal text-gray-400"> {{ position }}</Text>
+    </div>
+  </div>
 </template>
 <script lang="ts" setup>
-import { defineProps } from 'vue';
+import {defineProps} from 'vue';
 import Text from '@/components/Text.vue';
-const { writer, position, imgUrl  }  = defineProps<{ writer: string, position: string, imgUrl: string}>()
+
+const {writer, position, imgUrl} = defineProps<{ writer: string, position: string, imgUrl: string }>()
 </script>
